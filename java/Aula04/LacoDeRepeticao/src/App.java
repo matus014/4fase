411290dd91e71494a8b098bbcf98be6b2b62ec2a
MATEUS DE OLIVEIRA GOMES <mateus_o_gomes@estudante.sesisenai.org.br>
2024-02-19 21:45:37 -0300

    aula04

--- java/Aula04/LacoDeRepeticao/src/App.java
+++ java/Aula04/LacoDeRepeticao/src/App.java
@@ -9,7 +9,7 @@ public class App {
 
         for(int i = 1 ; i <= 10; i++)
         {
-            System.out.println(num +"x " + i + " = " + (num*i));
+            System.out.println(num +" x " + i + " = " + (num*i));
         }
 
     }
