ddc1f3201d7cc95c1ffc164cafaecb202f137327
MATEUS DE OLIVEIRA GOMES <mateus_o_gomes@estudante.sesisenai.org.br>
2024-02-21 21:57:27 -0300

    receitas

---
+++ front/Aula03-FRONT/Receitas/Funções/Botoes.js
@@ -0,0 +1,19 @@
+let receita= {
+
+    nome:"",
+    preparo:"",
+    ingrediente_1:"",
+
+}
+
+
+function Cadastrar(){}
+
+function PaginaExcluir(){}
+function PaginaPesquisar(){}
+function PaginaCadastro(){}
+
+
+function Add(){
+
+}
